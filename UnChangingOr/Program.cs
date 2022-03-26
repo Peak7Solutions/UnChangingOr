a67c7e74d36d48a518a2e0e2697a84da2be6bd72
Roger Huff <roger@smartgroupconsulting.us>
2022-03-26 19:36:43 -0400

    created initial solution

---
+++ UnChangingOr/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnChangingOr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
